1892dd191a7eded915f8d183073d48915af25407
Emannuel Morais <emanuelmorais2015@gmail.com>
2023-05-10 09:34:22 -0300

    Domain Model

--- src/main/java/com/devsuperior/dslist/repository/GameRepository.java
+++ src/main/java/com/devsuperior/dslist/repository/GameRepository.java
@@ -2,7 +2,9 @@ package com.devsuperior.dslist.repository;
 
 import com.devsuperior.dslist.entities.Game;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface GameRepository extends JpaRepository<Game, Long> {
 
 }
